ee80fa30fc0658cadd8aa281c7acfd595e2d623f
TobiasMue <muench.tm@googlemail.com>
2016-11-04 14:45:27 +0100

    try to load data from main.cpp
    Signed-off-by: TobiasMue <muench.tm@googlemail.com>

--- ACPerfSol/main.cpp
+++ ACPerfSol/main.cpp
@@ -1,11 +1,35 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <iostream>
+#include "Accel/Accel.h"
+#include "Climb/Climb.h"
+#include "Cruise/Cruise.h"
+#include "Descent/Descent.h"
+#include "Misc/RouteFunctions.h"
+#include "Misc/Wind.h"
+#include "Misc/ISA.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    // try to load data for testing
+
+    TClimbDataSet ClimbData(1);
+    ClimbData.LoadData("/Data/Climbdata.dat", ';', '\n');
+    std::cout << "Climb Loaded..." << std::endl;
+    TAccelDataSet AccelData(1);
+    AccelData.LoadData("/Data/Acceldata.dat", ';', '\n');
+    std::cout << "Accel Loaded..." << std::endl;
+    TCruiseDataSet CruiseData(1);
+    CruiseData.LoadData("/Data/Cruisedata.dat", ';', '\n');
+    std::cout << "Cruise Loaded..." << std::endl;
+    TDescentDataSet DescentData(1);
+    DescentData.LoadData("/Data/Descentdata.dat", ';', '\n');
+    std::cout << "Descent Loaded..." << std::endl;
 
     return a.exec();
 }
