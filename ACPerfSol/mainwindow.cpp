9188c9d488f4290f23b7f6205903a7af93435e0b
TobiasMue <muench.tm@googlemail.com>
2016-11-21 15:54:59 +0100

    example for plotting
    Signed-off-by: TobiasMue <muench.tm@googlemail.com>

--- ACPerfSol/mainwindow.cpp
+++ ACPerfSol/mainwindow.cpp
@@ -1,12 +1,63 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+//plot
+#include <QDesktopWidget>
+#include <QScreen>
+#include <QMessageBox>
+#include <QMetaEnum>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setGeometry(400, 250, 542, 390);
+    setupLineStyleDemo(ui->customPlot);
+    setWindowTitle("QCustomPlot: "+demoName);
+    statusBar()->clearMessage();
+    ui->customPlot->replot();
 }
+
+void MainWindow::setupLineStyleDemo(QCustomPlot *customPlot)
+{
+  demoName = "Line Style Demo";
+  customPlot->legend->setVisible(true);
+  customPlot->legend->setFont(QFont("Helvetica", 9));
+  QPen pen;
+  QStringList lineNames;
+  lineNames << "lsNone" << "lsLine" << "lsStepLeft" << "lsStepRight" << "lsStepCenter" << "lsImpulse";
+  // add graphs with different line styles:
+  for (int i=QCPGraph::lsNone; i<=QCPGraph::lsImpulse; ++i)
+  {
+    customPlot->addGraph();
+    pen.setColor(QColor(qSin(i*1+1.2)*80+80, qSin(i*0.3+0)*80+80, qSin(i*0.3+1.5)*80+80));
+    customPlot->graph()->setPen(pen);
+    customPlot->graph()->setName(lineNames.at(i-QCPGraph::lsNone));
+    customPlot->graph()->setLineStyle((QCPGraph::LineStyle)i);
+    customPlot->graph()->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, 5));
+    // generate data:
+    QVector<double> x(15), y(15);
+    for (int j=0; j<15; ++j)
+    {
+      x[j] = j/15.0 * 5*3.14 + 0.01;
+      y[j] = 7*qSin(x[j])/x[j] - (i-QCPGraph::lsNone)*5 + (QCPGraph::lsImpulse)*5 + 2;
+    }
+    customPlot->graph()->setData(x, y);
+    customPlot->graph()->rescaleAxes(true);
+  }
+  // zoom out a bit:
+  customPlot->yAxis->scaleRange(1.1, customPlot->yAxis->range().center());
+  customPlot->xAxis->scaleRange(1.1, customPlot->xAxis->range().center());
+  // set blank axis lines:
+  customPlot->xAxis->setTicks(false);
+  customPlot->yAxis->setTicks(true);
+  customPlot->xAxis->setTickLabels(false);
+  customPlot->yAxis->setTickLabels(true);
+  // make top right axes clones of bottom left axes:
+  customPlot->axisRect()->setupFullAxesBox();
+}
+
 
 MainWindow::~MainWindow()
 {
